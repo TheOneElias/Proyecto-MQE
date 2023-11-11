2f2b872576eb0090ecaa79b62857fc2ce700d33c
Marwich31 <149181867+Marwich31@users.noreply.github.com>
2023-11-10 22:32:42 -0600

    Add files via upload

---
+++ ejercicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mhm</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
+
+<!--php-->
+
+<?php
+$name ="Mozo";
+$age = 17;
+$est =1.50;
+
+echo "su nombre es: $name";
+echo"<br>";
+echo "su edad es: $age";
+echo"<br>";
+if($age >=18){
+    echo"ya estas viejo mijo";
+    echo"<br>";
+}
+else{
+    echo"tas muy chavito";
+    echo"<br>";
+}
+echo "su estatura es: $est";
+?>
